c3a1c11c8a902018239f1fd29072df90d8fe99c9
Riya Gupta <gupta.riya0911@gmail.com>
2019-01-17 22:15:40 +0530

    integrate login mutation.

--- react-redux/src/components/Login.js
+++ react-redux/src/components/Login.js
@@ -1,36 +1,84 @@
 import React, { Component } from "react";
 // import logo from "./logo.svg";
 // import "./App.css";
+import gql from "graphql-tag";
+import { graphql, compose, withApollo } from "react-apollo";
+
+export const SIGNIN_USER = gql`
+  mutation signinUser($username: String!, $password: String!) {
+    signinUser(username: $username, password: $password) {
+      token
+    }
+  }
+`;
 
 class Login extends Component {
   state = {
     username: "",
     password: ""
   };
+
+  handleLogin = (e) => {
+    const { name, value } = e.target
+    this.setState({ [name]: value })
+
+  }
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const { username, password } = this.state
+    const payload = {
+      username, password
+    }
+    this.props.loginUser(payload)
+  }
+
   render() {
     const { username, password } = this.state;
     return (
       <div>
         <p>Login</p>
-        <label>Username</label>
-        <input
-          label="username"
-          name={"username"}
-          value={username}
-          onChange={this.handleLogin}
-        />{' '}
-        <label>Password</label>
-        <input
-          label="password"
-          name={"password"}
-          value={password}
-          onChange={this.handleLogin}
-        />{' '}
-        <br/><br/>
-        <button onClick={this.onLogin}> Login </button>
+
+        <form
+          onSubmit={e=>this.handleSubmit(e)}
+        >
+          <label>Username</label>
+          <input
+            label="username"
+            name={"username"}
+            value={username}
+            onChange={this.handleLogin}
+          />{' '}
+          <label>Password</label>
+          <input
+            label="password"
+            name={"password"}
+            value={password}
+            onChange={this.handleLogin}
+          />{' '}
+          <br /><br />
+          <button onClick={this.onLogin}> Login </button>
+        </form>
       </div>
     );
   }
 }
 
-export default Login;
+
+
+
+const LoginWithData = withApollo(
+  compose(
+    graphql(SIGNIN_USER, {
+      props: ({ mutate }) => ({
+        loginUser(data) {
+          return mutate({
+            variables: { data }
+          })
+        }
+      })
+    })
+  )(Login)
+)
+
+
+export default LoginWithData;
--- react-redux/src/index.js
+++ react-redux/src/index.js
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom';
 // import '../src/style';
 import App from './components';
 import * as serviceWorker from './serviceWorker';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+    uri: "http://localhost:4444/graphql"
+});
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
